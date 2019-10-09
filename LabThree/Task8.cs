121231236fa8b348ecbf493deb35d8beb6178f49
Michael Melikov <coutaq@gmail.com>
2019-10-09 14:44:23 +0300

    still doesnt work

--- LabThree/Task8.cs
+++ LabThree/Task8.cs
@@ -14,19 +14,22 @@ namespace LabThree
                 for (int j = 0; j <= Math.Sqrt(array.Length) - 1; j++)
                 {
                     Random rnd = new Random();
-                    //array[i, j] = rnd.Next(0, 10);
-                    array[i, j] = i+j;
+                    array[i, j] = rnd.Next(0, 10);
                     Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(FindMOfIJ(0,0,array));
+            Console.WriteLine(FindDeterminant(array));
 
         }
-        //i actually know what to name this - findDeterminant  - but thats too boring
-        static int ILegitDontKnowWhatToNameThis(int[,] Matrix)
+        static double FindDeterminant(int[,] Matrix)
         {
-            return 0;
+            double detA = 0;
+            for (int k = 1; k < Math.Sqrt(Matrix.Length); k++)
+            {
+                detA += Matrix[1, k] * FindAOfIJ(1, k, Matrix);
+            }
+            return detA;
         }
         static double FindAOfIJ(int i, int j, int[,] matrix)
         {
@@ -36,20 +39,10 @@ namespace LabThree
         private static double FindMOfIJ(int i, int j, int[,] matrix)
         {
             int[,] new_matrix = new int[(int)(Math.Sqrt(matrix.Length) - 1), (int)(Math.Sqrt(matrix.Length) - 1)];
-            new_matrix = TrimArray(i, j, matrix);
-            Console.WriteLine("The matrix: ");
-            for (int l = 0; l <= Math.Sqrt(new_matrix.Length) - 1; l++)
-            {
-                for (int m = 0; m <= Math.Sqrt(new_matrix.Length) - 1; m++)
-                {
-                    Console.Write(new_matrix[l, m] + " ");
-                }
-                Console.WriteLine();
-            }
+            new_matrix = TrimArray(i, j, matrix);  
             if (new_matrix.Length == 4)
             {
-                Console.WriteLine(matrix[0,0]+"*"+matrix[1,1]+"-"+matrix[0,1]+"*"+matrix[1,0]);
-                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+                return new_matrix[0, 0] * new_matrix[1, 1] - new_matrix[0, 1] * new_matrix[1, 0];
             }
             else
             {
@@ -58,13 +51,10 @@ namespace LabThree
                 {
                     if (k % 2 != 0)
                     {
-
-                        Console.WriteLine("Determinant = " + determinant + " - " + new_matrix[0, k] + "*" + FindMOfIJ(k, 0, new_matrix));
                         determinant -= new_matrix[0, k] * FindMOfIJ(k, 0, new_matrix);
                     }
                     else
                     {
-                        Console.WriteLine("Determinant = " + determinant + " + " + new_matrix[0, k] + "*" + FindMOfIJ(k, 0, new_matrix));
                         determinant += new_matrix[0, k] * FindMOfIJ(k, 0, new_matrix);
                     }
                 }
